a1f1daddd657c73132995582aad82ad4f9c6f95e
Igor Gentil <igorlg@amazon.com>
2023-08-07 14:19:17 +1000

    chore: fix imports, add test function reload and rename test and event file for properties_service.contract_status_changed_event_handler

--- unicorn_properties/tests/unit/test_contract_status_changed_event_handler.py
+++ unicorn_properties/tests/unit/test_contract_status_changed_event_handler.py
@@ -1,12 +1,13 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-# import json
 import os
+# import json
+from importlib import reload
+
 # import pytest
 from unittest import mock
-from importlib import reload
-from moto import mock_dynamodb, mock_events
+# from moto import mock_dynamodb, mock_events
 
 # from aws_lambda_powertools.event_handler.exceptions import InternalServerError
 # from botocore.exceptions import ClientError
@@ -16,10 +17,13 @@ from .helper import load_event, return_env_vars_dict, create_ddb_table_propertie
 
 
 @mock.patch.dict(os.environ, return_env_vars_dict(), clear=True)
-def test_handle_contract_status_changed_event(dynamodb, mocker):
+def test_contract_status_changed_event_handler(dynamodb, mocker):
     eventbridge_event = load_event('tests/events/lambda/contract_status_changed.json')
 
     from properties_service import contract_status_changed_event_handler
+    # Reload is required to prevent function setup reuse from another test 
+    reload(contract_status_changed_event_handler)
+
     create_ddb_table_properties(dynamodb)
 
     context = LambdaContext()
