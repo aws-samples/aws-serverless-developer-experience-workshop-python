04d5933ff3cca956dced68cefcf5fb6221654d81
Igor Gentil <igorlg@amazon.com>
2023-08-25 17:38:09 +1000

    feat: Refactored unicorn_web

--- unicorn_contracts/src/contracts_service/enums.py
+++ unicorn_contracts/src/contracts_service/enums.py
@@ -9,9 +9,12 @@ class ContractStatus(Enum):
  
     APPROVED 	The contract record is approved.
     CANCELLED   The contract record is canceled or terminated. You cannot modify a contract record that has this status value.
-    CLOSED 	    The contract record is closed and all its terms and conditions are met. You cannot modify a contract record that has this status value.
+    CLOSED 	    The contract record is closed and all its terms and conditions are met.
+                You cannot modify a contract record that has this status value.
     DRAFT 	    The contract is a draft.
-    EXPIRED 	The contract record is expired. The end date for the contract has passed. You cannot modify a contract record that has this status value. You can change the status from expire to pending revision by revising the expired contract.
+    EXPIRED 	The contract record is expired. The end date for the contract has passed.
+                You cannot modify a contract record that has this status value.
+                You can change the status from expire to pending revision by revising the expired contract.
  
     Parameters
     ----------
