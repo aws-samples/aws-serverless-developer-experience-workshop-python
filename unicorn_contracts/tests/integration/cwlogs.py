e96e9c9724092dc8652bfd839e430cd0f411ad11
Igor Gentil <igorlg@amazon.com>
2023-08-31 14:37:56 +1000

    chore: add missing licensing header

--- unicorn_contracts/tests/integration/cwlogs.py
+++ unicorn_contracts/tests/integration/cwlogs.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
 import json
 from datetime import datetime, timedelta
 from time import sleep, time
