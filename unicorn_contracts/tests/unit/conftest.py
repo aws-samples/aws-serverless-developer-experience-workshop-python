89019c57aea6cd4cbabfe51bd76636240ae81261
Igor Gentil <igorlg@users.noreply.github.com>
2023-09-11 10:01:27 +0000

    Lint code (#43)
    * chore: remove unused code and forgotten comments
    
    * chore: reorder ENV vars; make LambdaContext mock generic to fit all functions
    
    * chore: lint Python files
    
    * test: add tracer and logger output on propertes_approval_sync_function methods
    
    * chore: reformat StateMachine ASL file
    
    * fix: update Stage name in shared stack
    
    * chore: renamed stack file for unicorn_shared

--- unicorn_contracts/tests/unit/conftest.py
+++ unicorn_contracts/tests/unit/conftest.py
@@ -19,18 +19,6 @@ def aws_credentials():
 
 
 @pytest.fixture(scope='function')
-def env_vars():
-    os.environ['POWERTOOLS_SERVICE_NAME']='unicorn.contracts'
-    os.environ['SERVICE_NAMESPACE']='unicorn.contracts'
-    os.environ['POWERTOOLS_SERVICE_NAME']='unicorn.contracts'
-    os.environ['POWERTOOLS_TRACE_DISABLED']='true'
-    os.environ['POWERTOOLS_LOGGER_LOG_EVENT']='Info'
-    os.environ['POWERTOOLS_LOGGER_SAMPLE_RATE']='0.1'
-    os.environ['POWERTOOLS_METRICS_NAMESPACE']='unicorn.contracts'
-    os.environ['LOG_LEVEL']='INFO'
-
-
-@pytest.fixture(scope='function')
 def dynamodb(aws_credentials):
     with mock_dynamodb():
         yield boto3.resource('dynamodb', region_name='ap-southeast-2')
@@ -51,12 +39,12 @@ def sqs(aws_credentials):
 @pytest.fixture(scope='function')
 def lambda_context():
     context: LambdaContext = LambdaContext()
-    context._function_name="contractsService-CreateContractFunction-IWaQgsTEtLtX"
+    context._function_name="contractsService-LambdaFunction-IWaQgsTEtLtX"
     context._function_version="$LATEST"
-    context._invoked_function_arn="arn:aws:lambda:ap-southeast-2:424490683636:function:contractsService-CreateContractFunction-IWaQgsTEtLtX"
+    context._invoked_function_arn="arn:aws:lambda:ap-southeast-2:424490683636:function:contractsService-LambdaFunction-IWaQgsTEtLtX"
     context._memory_limit_in_mb=128
     context._aws_request_id="6f970d26-71d6-4c87-a196-9375f85c7b07"
-    context._log_group_name="/aws/lambda/contractsService-CreateContractFunction-IWaQgsTEtLtX"
+    context._log_group_name="/aws/lambda/contractsService-LambdaFunction-IWaQgsTEtLtX"
     context._log_stream_name="2022/07/14/[$LATEST]7c71ca59882b4c569dd007c7e41c81e8"
     # context._identity=CognitoIdentity([cognito_identity_id=None,cognito_identity_pool_id=None])])
     # context._client_context=None
