ab6fe690b208b9692ac6fa5ee7ce08a3c75b9df8
Igor Gentil <igorlg@amazon.com>
2023-09-11 19:42:27 +1000

    test: reorganised test events, refactored unit tests

--- unicorn_properties/tests/unit/test_properties_approval_sync_function.py
+++ unicorn_properties/tests/unit/test_properties_approval_sync_function.py
@@ -1,37 +1,35 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-
 import os
 from importlib import reload
 
 from unittest import mock
 
-from .lambda_context import LambdaContext
 from .helper import load_event, return_env_vars_dict
 
 
 @mock.patch.dict(os.environ, return_env_vars_dict(), clear=True)
-def test_handle_status_changed_draft(stepfunction, mocker):
-    ddbstream_event = load_event('tests/events/dbb_stream_events/contract_status_changed_draft.json')
+def test_handle_status_changed_draft(stepfunction, lambda_context):
+    ddbstream_event = load_event('ddb_stream_events/contract_status_changed_draft')
 
     from properties_service import properties_approval_sync_function
     reload(properties_approval_sync_function)
 
-    ret = properties_approval_sync_function.lambda_handler(ddbstream_event, LambdaContext())
+    ret = properties_approval_sync_function.lambda_handler(ddbstream_event, lambda_context)
 
     assert ret is None
 
 
 # NOTE: This test cannot be implemented at this time because `moto`` does not yet support mocking `stepfunctions.send_task_success`
 @mock.patch.dict(os.environ, return_env_vars_dict(), clear=True)
-def test_handle_status_changed_approved(caplog, stepfunction, mocker):
+def test_handle_status_changed_approved(caplog, stepfunction, lambda_context):
     pass
-    # ddbstream_event = load_event('tests/events/dbb_stream_events/status_approved_waiting_for_approval.json')
+    # ddbstream_event = load_event('ddb_stream_events/status_approved_waiting_for_approval')
 
     # from properties_service import properties_approval_sync_function
     # reload(properties_approval_sync_function)
 
-    # ret = properties_approval_sync_function.lambda_handler(ddbstream_event, LambdaContext())
+    # ret = properties_approval_sync_function.lambda_handler(ddbstream_event, lambda_context)
 
     # assert ret is None
     # assert 'Contract status for property is APPROVED' in caplog.text
