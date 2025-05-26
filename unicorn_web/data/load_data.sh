815eb834f6a9aab97e02d4e882b15514724c0763
adrianjhunter <4285633+adrianjhunter@users.noreply.github.com>
2025-05-26 17:30:58 +1000

    feat: implements jsonata workflow, replaces YAML SAM Config with TOML, removes AWS::LanguageExtensions

--- unicorn_web/data/load_data.sh
+++ unicorn_web/data/load_data.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 ROOT_DIR="$(cd -- "$(dirname "$0")/../" >/dev/null 2>&1 ; pwd -P )"
-STACK_NAME="$(yq -oy '.default.global.parameters.stack_name' $ROOT_DIR/samconfig.yaml)"
+STACK_NAME="$(yq -ot '.default.global.parameters.stack_name' $ROOT_DIR/samconfig.toml)"
 
 JSON_FILE="$ROOT_DIR/data/property_data.json"
 echo "JSON_FILE: '${JSON_FILE}'"
 
-DDB_TBL_NAME="$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query 'Stacks[0].Outputs[?OutputKey==`WebTableName`].OutputValue' --output text)"
+DDB_TBL_NAME="$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query 'Stacks[0].Outputs[?ends_with(OutputKey, `WebTableName`)].OutputValue' --output text)"
 echo "DDB_TABLE_NAME: '${DDB_TBL_NAME}'"
 
 echo "LOADING ITEMS TO DYNAMODB:"
