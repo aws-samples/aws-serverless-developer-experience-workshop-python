66c059975810c519c6a72feb14234072e5d99e36
Stephen Liedig <sliedig@amazon.com>
2025-09-06 10:19:45 +0800

    refactor: renamed WebTable to Properties table to match architecture

--- unicorn_web/data/load_data.sh
+++ unicorn_web/data/load_data.sh
@@ -6,7 +6,7 @@ STACK_NAME="$(yq -ot '.default.global.parameters.stack_name' $ROOT_DIR/samconfig
 JSON_FILE="$ROOT_DIR/data/property_data.json"
 echo "JSON_FILE: '${JSON_FILE}'"
 
-DDB_TBL_NAME="$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query 'Stacks[0].Outputs[?ends_with(OutputKey, `WebTableName`)].OutputValue' --output text)"
+DDB_TBL_NAME="$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query 'Stacks[0].Outputs[?ends_with(OutputKey, `PropertiesTableName`)].OutputValue' --output text)"
 echo "DDB_TABLE_NAME: '${DDB_TBL_NAME}'"
 
 echo "LOADING ITEMS TO DYNAMODB:"
